1d1d511d96def3598c9748bc963c4e2e962eb1d6
Raghupathy <ragupathi.kommidi@gmail.com>
2022-05-17 16:08:15 +0530

    changes

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -13,8 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
     
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
+        SpringApplication.run(Application.class, args);     
     }
 
 }
